e523d6868c16d10d97651f27f08045416e773bab
zevdvlpr <zevdvlpr@gmail.com>
2020-09-08 13:55:46 -0300

    core: channellist

--- src/pages/Following/index.tsx
+++ src/pages/Following/index.tsx
@@ -5,6 +5,7 @@ import Heading from "../../components/Heading";
 import Title from "../../components/Title";
 import CategoryList from "../../components/CategoryList";
 import StreamList from '../../components/StreamList';
+import ChannelList from '../../components/ChannelList';
 
 import { Wrapper, Container, Main } from "./styles";
 import { View, FlatList } from "react-native";
@@ -49,7 +50,7 @@ const Following: React.FC = () => {
         render: () => <Title>Offline Channels</Title>,
         isTitle: true,
       },
-      { key: "C4", render: () => <View /> },
+      { key: "C4", render: () => <ChannelList /> },
     ];
 
     const indices: number[] = [];
